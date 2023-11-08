f1021ca52c7505636e6abbe04075334d56b259ca
mamunhero <96989895+mamunhero@users.noreply.github.com>
2023-11-09 00:26:49 +0600

    make room detailes id

--- src/Pages/Rooms/RoomCard.jsx
+++ src/Pages/Rooms/RoomCard.jsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 
 const RoomCard = ({roomInfo}) => {
-  const {image, roomType, price} = roomInfo || {}
+  const {_id, image, roomType, price} = roomInfo || {}
   return (
     <div className="mt-10 mb-10">
       <div className="card  bg-base-100 shadow-xl">
@@ -11,7 +12,9 @@ const RoomCard = ({roomInfo}) => {
     <h2 className="card-title">Room Type:{roomType}</h2>
     <p>Price: {price}</p>
     <div className="card-actions">
+     <Link to={`roomdetailes/${_id}`}>
       <button className="btn btn-primary btn-sm">Detailes</button>
+     </Link>
     </div>
   </div>
 </div>
--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -30,7 +30,8 @@ const Route = createBrowserRouter([
       {
         path:"/addroom",
         element: <AddRoom></AddRoom>
-      }
+      },
+      
     ],
   },
   {
